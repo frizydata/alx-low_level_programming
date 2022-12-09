e5929f788039de9c9bb76e71e918a6d3a16105fa
frizydata <ojokof@gmail.com>
2022-12-09 14:09:28 +0100

    Positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,21 +6,21 @@
  * main - Entry point
  * 
  * Return: Always return 0 (Success)
- * /
+ */
 
 int main(void)
 {
 	int n;
 
-	srand(time(0))
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0 ) {
+	if (n > 0) {
 		printf("%d is positive\n", n);
 	} else if (n == 0) {
 		printf("%d is zero\n", n);
 	} else {
-		printf("%d is negative\n");
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
