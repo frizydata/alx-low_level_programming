787729da312f82464d89a01d9d4bdb5263b77959
frizydata <ojokof@gmail.com>
2022-12-08 19:06:01 -0800

    Sizes of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Recovery: Always 0 (Success)
  */
 
 int main(void)
 {
-	
-	printf("Size of a char : %d byte(s)\n", sizeof(char));
-	printf("Size of an int : %d byte(s)\n", sizeof(int));
-	printf("Size of a long int : %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int : %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float : %d byte(s)\n", sizeof(float));
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d bytes(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 
 	return (0);
 
