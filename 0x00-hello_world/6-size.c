fac0d2feda15860c85d68585221e14c0e589e44e
frizydata <ojokof@gmail.com>
2022-12-08 20:22:41 -0800

    Commit this

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,14 @@ int main(void)
 {
 	char c;
 	int i;
-	long int li;
+	long int l;
 	long long int ll;
 	float f;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long int: %d byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
-	
+	printf("Size of a float: %d byte(s)\n", sizeof(f));	
 	return (0);
 }
