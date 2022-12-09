d5b6a55a5ec63142782cd2c13d5f7a5c57ca26ac
frizydata <ojokof@gmail.com>
2022-12-08 20:14:48 -0800

    Commit this

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
 	printf("Size of a float: %d byte(s)\n", sizeof(f));
-
 	return (0);
 }
