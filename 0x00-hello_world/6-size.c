4db4dbc7a2273a04fe7ac34ec7ba6c5b6a8d62a1
frizydata <ojokof@gmail.com>
2022-12-08 16:08:13 -0800

    size of various types of computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,21 @@
  * Return: Always 0 (success)
  */
 
+main(void)
+
 int main(void)
 {
 
-printf("Size of char: %lu \n", sizeof(c));
-printf("Size of int: %lu \n", sizeof(i));
-printf("Size of long int: %lu \n" sizeof(i));
-printf("Size of long long int: %lu \n", sizeof(i));
-printf("Size of float: %lu \n", sizeof(f));
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("Size of char: %lu byte(s) \n", sizeof(c));
+printf("Size of int: %lu bytes(s) \n", sizeof(i));
+printf("Size of long int: %lu bytes(s) \n" sizeof(li));
+printf("Size of long long int: %lu (bytes) \n", sizeof(lli));
+printf("Size of float: %lu byte(s)\n", sizeof(f));
 
 return (0);
 
