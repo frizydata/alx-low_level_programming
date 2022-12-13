f94039fdbcacabff00400e2af6b4660f284e4502
frizydata <ojokof@gmail.com>
2022-12-14 00:11:41 +0100

    Checks if character is alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * int _isalpha(int c) - Checks if c is a letter, lowercase or uppercase
+ * _isalpha(int c) - Checks if c is a letter, lowercase or uppercase
  * @c: The alphabet to check
  * Return: 1 if c is letter, lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	return((c > 64 && c < 91) || (c > 96 && c < 123));
+	return ((c > 64 && c < 91) || (c > 96 && c < 123));
 }
