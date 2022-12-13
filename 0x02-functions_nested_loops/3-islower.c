90bde005a972586204f1a49c7446bb944adf519b
frizydata <ojokof@gmail.com>
2022-12-13 23:44:46 +0100

    Checks if the alphabets is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main"
+/**
+ * _islower - Checks if the alphabet is lower
+ * @c: the alphabet to be checked
+ * Return: 1 if c is lowercase and 0 if otherwise
+ */
+
+int _islower(int c)
+{
+ 	return (c > 97 && c < 122);
+}
