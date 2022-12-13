791371325eb9673b06f99cefd52f5fa64dbe8523
frizydata <ojokof@gmail.com>
2022-12-13 22:33:48 +0100

    This function the alphabets ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - This function prints alphabeths ten times in lowercase
+ * repeat_alphabet - This function prints alphabeths ten times in lowercase
  * Return - Always 0 (Success)
  */
 
@@ -24,10 +24,4 @@ void repeat_alphabeth(void)
 
 	}
 
-}
-
-int main(void)
-{
-	repeat_alphabeth();
-	return (0);
 }
