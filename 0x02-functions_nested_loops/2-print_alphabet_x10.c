fca94f41a9f9639c29ae79a315212b7dd9a5037d
frizydata <ojokof@gmail.com>
2022-12-13 20:00:12 +0100

    This function prints alphabeis function prints alphabeths ten times in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - This function prints alphabeths ten times in lowercase
+ * Return - Always 0 (Success)
+ */
+
+void repeat_alphabeth(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+
+	while (i <= 9 )
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+
+		i++;
+
+	}
+
+}
