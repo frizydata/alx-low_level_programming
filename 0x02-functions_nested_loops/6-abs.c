899a77f0b0daea3aea2ff77fc880c5f0ca3ce32e
frizydata <ojokof@gmail.com>
2022-12-14 00:57:20 +0100

    Computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.c"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to be computed
+ * Return: Always 0 (Successful)
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = (-1) * n;
+	}
+	cout << " " n;
+}
