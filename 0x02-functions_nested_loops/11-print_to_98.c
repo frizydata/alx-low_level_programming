f47c8e87954ee81ffb678a5455dcc470ff426ec1
frizydata <ojokof@gmail.com>
2022-12-14 04:39:12 +0100

    Print natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-/*print_to_98 - Prints all natural numbers from n to 98
- * n: The start point 
+/*
+ * print_to_98 - Prints all natural numbers from n to 98
+ * n: The start point
  * Return: Always 0 (Success)
  */
 
