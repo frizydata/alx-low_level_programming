836e824c4f4171d7e3213250b42026328581b1c1
frizydata <ojokof@gmail.com>
2022-12-14 03:22:12 +0100

    The 9 times table starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int a; b; c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = b * a;
+
+			if (b == 0)
+			{
+				_putchar(c + '0' );
+			}
+
+
+			if (c <= 9 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10 +'0'))
+			}
+		}
+
+		_putchar('\n');
+	}
+}
+
