f04e0945eab382bff881f539d78b9cae5e27dea5
frizydata <ojokof@gmail.com>
2022-12-14 03:44:01 +0100

    The 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -36,7 +36,7 @@ void times_table(void)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((c / 10) + '0');
-					_putchar((c % 10) +'0');
+					_putchar((c % 10) + '0');
 				}
 			}
 			_putchar('\n');
