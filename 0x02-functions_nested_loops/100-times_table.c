9bbcd320f8123315160efa9583f9c60401e2ece0
frizydata <ojokof@gmail.com>
2022-12-14 05:44:26 +0100

    multiples of 3 and 5

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <=n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Print sum of multiples of 3 or 5 below 1024
+ * Return: int
+ */
+
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 0;
+	b = 0;
+	c = 0;
+
+	for (i = 0; i < 1024; ++1)
+	{
+		if ((i % 3) == 0)
+		{
+			a = a + 1;
+		}
+		else if ((i % 5) == 0)
+		{
+			b = b + 1;
+		}
+	}
+	c = a + b;
+	printf("%lu\n", c);
+	
+	return (0);
+}
