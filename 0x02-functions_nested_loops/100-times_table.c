342e6be2e9c3343a6efa6390c5f7dfe47d64eade
frizydata <ojokof@gmail.com>
2022-12-14 05:17:37 +0100

    Times Table up to if

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-				else if (k < 10 && j != 0);
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
