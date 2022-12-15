f72bb5f00972a3c2a75181f8f0fe467cf318eaec
frizydata <ojokof@gmail.com>
2022-12-15 12:30:18 -0800

    Check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * _isupper - Checks if a character is uppercase
+ * @c: The character to be checked
+ * Return: Always 1 if uppercase and 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	return ((c > 64) && (c < 91));
+}
