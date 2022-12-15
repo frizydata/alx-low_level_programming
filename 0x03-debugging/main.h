09cc4deb5b910bdd932724e2f9b019782026538d
frizydata <ojokof@gmail.com>
2022-12-15 08:12:40 -0800

    Largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,34 +5,6 @@
 
 void positive_or_negative(int i);
 
+int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
-
-#include "main.h"
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
-int largest_number(int a, int b, int c)
-{
-    int largest;
-
-    if (a >= b && a >= c)
-    {
-        largest = a;
-    }
-    else if (b >= a && b >= c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
