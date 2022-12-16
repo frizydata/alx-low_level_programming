46bc17aae074a5f6bd275db5d97c84ad01e89cfd
frizydata <ojokof@gmail.com>
2022-12-15 23:50:38 -0800

    Print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - prints integer
+ * @n: a given number
+ * Return: always integer
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
+}
