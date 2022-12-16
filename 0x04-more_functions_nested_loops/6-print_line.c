1bd0a22496cb3ab219d1d1f7690a905e416a4c77
frizydata <ojokof@gmail.com>
2022-12-15 16:47:03 -0800

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,23 @@
+#include "main.h"
+/*
+ * print_line - prints a line
+ *
+ * Return: Always 0
+ */
 
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar("_");
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-
-#endif
+void print_line(int n);
