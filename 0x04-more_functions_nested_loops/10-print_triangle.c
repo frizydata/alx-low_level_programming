4b48fcc28e4e90b7198cd8b6168df14afd15e67b
frizydata <ojokof@gmail.com>
2022-12-15 20:15:10 -0800

    Added some files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,4 +15,18 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (i = 
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
