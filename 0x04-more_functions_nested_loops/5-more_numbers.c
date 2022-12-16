5044314c4a092ad4b247ce37f03570940d7078c8
frizydata <ojokof@gmail.com>
2022-12-15 16:25:02 -0800

    Print numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
+}
