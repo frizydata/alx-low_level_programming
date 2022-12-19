aae4ceb609c0e0f26ba88e94242b17e6ac27bb0e
frizydata <ojokof@gmail.com>
2022-12-19 13:49:47 -0800

    This script swaps two values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - This function swaps the values of two ints
+ * @a: The value to swap to b
+ * @b: The value to swap to a
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = c;
+	*b = c;
+	*a = *b;
+}
