2c07635fbdc662be8d2f711191222122b8168fe6
frizydata <ojokof@gmail.com>
2022-12-19 02:02:42 -0800

    Size of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of an int: %d bytes(s)\n", sizeof(i));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
 	printf("Size of a float: %d byte(s)\n", sizeof(f));
