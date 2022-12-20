5281c112296b7dc922c58a0200241e3f8e976c01
frizydata <ojokof@gmail.com>
2022-12-20 00:05:07 -0800

    This script swaps values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,6 +3,7 @@
  * swap_int - This function swaps the values of two ints
  * @a: The value to swap to b
  * @b: The value to swap to a
+ * Return: The values of the ints exchanged
  */
 
 void swap_int(int *a, int *b)
