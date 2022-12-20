2668bbe96a41214d3df59efe151a19439e334c5d
frizydata <ojokof@gmail.com>
2022-12-20 02:04:34 -0800

    Added script

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,17 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	for (; i < n; i++)
+	for (i = 0; i < n; ++i)
 	{
-		printf("%d", *(a + 1));
-
 		if (i != (n - 1))
 		{
-			printf(", ");
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
 		}
 	}
 
