3616bb7cc27ab4c77b8c69e04c73e43e5b7471c7
frizydata <ojokof@gmail.com>
2022-12-22 14:42:45 -0800

    Comparing Strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,11 +3,11 @@
  * _strcmp - a function that compares two strings
  * @s1: string 1 input to compare
  * @s2: against this other string 2
- * Return: 0 if s1 and s2 are equal, 
+ * Return: 0 if s1 and s2 are equal,
  * negative integer if the stopping character
  * in @s1 was less than the stopping
  * character in @s2
- * positive integer if the stopping character in 
+ * positive integer if the stopping character in
  * @s1 was greater than the stopping character in @s2
  */
 
@@ -15,15 +15,13 @@ int _strcmp(char *s1, char *s2)
 {
 	unsigned char a, b;
 
-	do
-	{
+	do {
 		a = *s1++;
 		b = *s2++;
 
 		if (c1 == '\0')
 			return (c1 - c2);
-	}
-	while (c1 == c2);
-	
+	} while (c1 == c2);
+
 	return (output);
 }
