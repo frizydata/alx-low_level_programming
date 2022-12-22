ac4ebbe6337279d0c7c2515ce6a1ba1ca6c222f6
frizydata <ojokof@gmail.com>
2022-12-22 13:27:42 -0800

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,14 @@ char *_strcat(char *dest, char *src)
 	int a, b;
 
 	a = 0;
-
-	{while (dest[a])
+	while (dest[a])
+	{
 		a++;
 	}
 
 	for (b = 0; src[b]; b++)
 	{
-		dest[a++] = src[b++];
+		dest[a++] = src[b];
 	}
 
 	return (0);
