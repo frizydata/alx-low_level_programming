c26c96bf7ac3910d402917d9c364a772d3ffa2c8
frizydata <ojokof@gmail.com>
2022-12-24 00:15:16 -0800

    Rot13 encoding style

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using the rot13 style
+ * @s : Pointer to the string to encode
+ * Return: Pointer to string s
+ */
+
+char *rot13(char *s)
+{
+	int a, b = 0;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[a] != '\0')
+	{
+		for (b = 0; b <= 51; b++)
+		{
+			if (s[a] == alphabet[b])
+			{
+				s[a] = rot13_alphabet[b];
+				break;
+			}
+		}
+		a++;
+	}
+
+	return (s);
+}
