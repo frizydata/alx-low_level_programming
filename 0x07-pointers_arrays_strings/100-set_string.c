a70f78b3432924e6d930feeead74660a0a8cbce2
frizydata <ojokof@gmail.com>
2023-01-02 15:58:34 -0800

    Added function file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - Function that sets the value of a pointer to a char.
+ * @s: content to be copied
+ * @to: element to paste in
+ * Return: no returns
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
