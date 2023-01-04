77aa9182de5150c6c6a67c6a77ff1f3bf0b2e618
frizydata <ojokof@gmail.com>
2023-01-04 15:55:56 -0800

    Recussion in Ci

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - This function returns the value of x raised to the power of y
+ * _pow_recursion - This function returns the value of x raised to power of y
  * @x: Base number
  * @y: Exponent number
  * Return: Value of x raise to power of y
@@ -12,7 +12,7 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else if ((y == 0) || (y == 1))
+	else if (y == 0)
 	{
 		return (1);
 	}
