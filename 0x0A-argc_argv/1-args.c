7612ec1dd9d58c3fe46cae10d9d202a368e30955
frizydata <ojokof@gmail.com>
2023-01-07 02:43:22 +0100

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints number of arguments passed to it
+ * @argc: number of command line argument
+ * @argv: array that contains the program command line arguments
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
