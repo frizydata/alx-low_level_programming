deb22213ac61acb4bca59d2dca1a22957d84a796
frizydata <ojokof@gmail.com>
2023-01-07 04:04:46 +0100

    Adds Positive numbers, with condition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of command line argument
+ * @argv: array that contains the program command line arguments
+ * Return: 0 if no number is passed to the program,
+ * 	Error and 1 if one number contains symbol
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	print("%d\n", add);
+	return (0);
+}
