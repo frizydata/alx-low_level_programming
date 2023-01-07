3cf0be6ed0e8c66b042ee030a1597ef390527e43
frizydata <ojokof@gmail.com>
2023-01-07 03:09:23 +0100

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,10 +17,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		result = atoi(argv[1] * atoi(argv[2]));
-		printf("%d/n", result);
-		return (0);
-	}
+	printf("%d\n", atoi(argv[1] * atoi(argv[2]));
+	return (0);
 }
