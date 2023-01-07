56d5597b4655a85ab6e9fedade00e63bb4368b35
frizydata <ojokof@gmail.com>
2023-01-07 04:32:49 +0100

    Adds Positive numbers, with condition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,9 +17,13 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		add += atoi(argv[i]);
 	}
 	printf("%d\n", add);
 	return (0);
