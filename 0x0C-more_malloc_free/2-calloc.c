bc949930d3412c7ff2d0ad728257a0f93c180f12
frizydata <ojokof@gmail.com>
2023-01-12 16:32:29 -0800

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,6 +16,8 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
+
+
 }
 
 
@@ -24,7 +26,6 @@ char *_memset(char *s, char b, unsigned int n)
  * memory for an array using malloc
  * It is basically the equivalent to
  * malloc followed by memset
- *
  * @nmemb: size of array
  * @size: size of each element
  * Return: pointer with new allocated memory
@@ -37,11 +38,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
+	
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-
+	
 	_memset(p, 0, nmemb * size);
+	
 	return (p);
 }
