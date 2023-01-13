003cbd4dc2a8f3e339904b75d7f062f833e265e1
frizydata <ojokof@gmail.com>
2023-01-12 16:34:05 -0800

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -38,12 +38,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	
+
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	
+
 	_memset(p, 0, nmemb * size);
-	
+
 	return (p);
 }
