0651178da567be9fb9028d8116d4782f4bb000dd
frizydata <ojokof@gmail.com>
2023-01-16 17:51:57 +0100

    Puppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure definition
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
 struct dog 
 {
 	char *name;
 	float *age;
 	char *owner;
 };
+
+#endif
